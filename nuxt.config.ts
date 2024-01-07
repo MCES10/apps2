727ff2ffb53f60c17f9c506414c04e2fa77734ae
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-07 20:39:51 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { plugins } from "./mces10software/postcss.config"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare-pages'
+    preset: 'github-pages'
   },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
